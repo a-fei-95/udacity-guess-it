da556319dec5af60e77ad7fef8ec7bb661ba093e
Steven Yang <stevenwaterloo2015@gmail.com>
2023-03-24 21:19:08 -0400

    live data

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,15 +1,74 @@
 package com.example.android.guesstheword.screens.game
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import timber.log.Timber
 
 class GameViewModel : ViewModel() {
+
+    var word = MutableLiveData<String>()
+    var score = MutableLiveData<Int>()
+
+    private lateinit var wordList: MutableList<String>
+
     init {
         Timber.i("GameViewModel created!")
+
+        word.value = ""
+        score.value = 0
+
+        resetList()
+        nextWord()
     }
 
     override fun onCleared() {
         super.onCleared()
         Timber.i("GameViewModel destroyed!")
+    }
+
+    fun onSkip() {
+        score.value = score.value?.minus(1)
+        nextWord()
+    }
+
+    fun onCorrect() {
+        score.value = score.value?.plus(1)
+        nextWord()
+    }
+
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+//            gameFinished()
+        } else {
+            word.value = wordList.removeAt(0)
+        }
+    }
+
+    private fun resetList() {
+        wordList = mutableListOf(
+            "queen",
+            "hospital",
+            "basketball",
+            "cat",
+            "change",
+            "snail",
+            "soup",
+            "calendar",
+            "sad",
+            "desk",
+            "guitar",
+            "home",
+            "railway",
+            "zebra",
+            "jelly",
+            "car",
+            "crow",
+            "trade",
+            "bag",
+            "roll",
+            "bubble"
+        )
+        wordList.shuffle()
     }
 }
